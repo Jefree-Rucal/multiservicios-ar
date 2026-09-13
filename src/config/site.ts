f2830b9fbd2319c9731f3cfd31d5ce258c41a5dd
Jefree Rucal <Jefreealejandro1190@gmail.com>
2026-09-13 08:43:18 -0600

    Modularizar estructura de la Home

---
+++ src/config/site.ts
@@ -0,0 +1,29 @@
+    export const site = {
+	name: "Multiservicios Obra Civil AR",
+	shortName: "AR",
+	country: "Guatemala",
+
+	description:
+		"Obra civil, suministros hidráulicos y Deck WPC para empresas, proyectos y clientes en Guatemala.",
+
+	quoteHref: "/#contacto",
+
+	navigation: [
+		{
+			label: "Servicios",
+			href: "/#servicios",
+		},
+		{
+			label: "Hidráulicos",
+			href: "/#hidraulicos",
+		},
+		{
+			label: "Deck WPC",
+			href: "/#deck",
+		},
+		{
+			label: "Proyectos",
+			href: "/#proyectos",
+		},
+	],
+};
