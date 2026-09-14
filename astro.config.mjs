bce2d0c081cd0c626465d81c3c701246333d2ec6
Jefree Rucal <Jefreealejandro1190@gmail.com>
2026-09-13 20:25:12 -0600

    Configurar dominio y sitemap para produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-// Cuando tengamos el dominio final agregaremos:
-//
-// site: "https://www.DOMINIO.com",
-//
-// y activaremos el sitemap.
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+	site: "https://obracivilar.com",
 	trailingSlash: "ignore",
+	integrations: [sitemap()],
 });
